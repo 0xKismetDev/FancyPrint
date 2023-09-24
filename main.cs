31de8bf15e5e34e6a847b41b223ba4617c4997d3
KismetDev <131729061+0xKismetDev@users.noreply.github.com>
2023-09-24 19:47:26 +0200

    main files

---
+++ main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FancyPrint
+{
+    public class fPrint
+    {
+        public static void Info(string message)
+        {
+            Console.WriteLine("[i] {0}", message);
+        }
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[-] {0}", message);
+            Console.ResetColor();
+        }
+
+        public static void Okay(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[+] {0}", message);
+            Console.ResetColor();
+        }
+
+        public static void Warn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[!] {0}", message);
+            Console.ResetColor();
+        }
+
+
+    }
+}
